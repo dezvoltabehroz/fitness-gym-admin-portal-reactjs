2d29dd975355db3f7773708a35fdb792b605d789
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-04-09 13:32:10 +0500

    hot fixes

--- src/jsx/components/EducoGym/config.js
+++ src/jsx/components/EducoGym/config.js
@@ -1,19 +1,25 @@
 module.exports = {
-    method_post: { "method": "POST" },
-    method_get: { "method": "GET" },
-  
-    // API call with Body Parameter
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  
-    axios_config: {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjE3NzM4NDg0LCJleHAiOjE2MTc4MjQ4ODR9.dtxush27S1Zbh4JkoagbROPmW7q2iJU6BF4bUdrkGXk'
-      },
+  method_post: { "method": "POST" },
+  method_get: { "method": "GET" },
+
+  // API call with Body Parameter
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+
+  axios_config: {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjE3OTUyODg0LCJleHAiOjE2MTgwMzkyODR9.fEowp_JJTU9ZuU4xSE9jsrcub0qia0jBL0ZPtXbMNi0'
     },
-  
-    api_base_url: 'http://18.204.20.183:3005/api/' // Development
-  }
-  
+  },
+  requestOptions: {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjE3OTUyODg0LCJleHAiOjE2MTgwMzkyODR9.fEowp_JJTU9ZuU4xSE9jsrcub0qia0jBL0ZPtXbMNi0'
+    }
+  },
+  api_base_url: 'http://18.204.20.183:3005/api/' // Development
+}
