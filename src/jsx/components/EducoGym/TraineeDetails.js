4fdf3c72da9eb5aa2c0d9e3280863ca79515b086
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-04-21 01:05:33 +0500

    changes

--- src/jsx/components/EducoGym/TraineeDetails.js
+++ src/jsx/components/EducoGym/TraineeDetails.js
@@ -59,7 +59,9 @@ class TraineeDetails extends React.Component {
                this.setState({ members: [], isLoading: false })
             }
          })
-         .catch((error) => { // alert(error) })
+         .catch((error) => { 
+            // alert(error) 
+         })
    }
 
    fetchMemberDetail = (id) => {
@@ -120,12 +122,12 @@ class TraineeDetails extends React.Component {
                await this.fetchMemberList()
             }
             else {
-               // alert(res.message)
+               alert(res.message)
                // this.setState({ memberDetail: {}, isLoading: false })
             }
          })
          .catch((error) => {
-            // alert(error)
+            alert(error)
          })
    }
 
@@ -155,12 +157,12 @@ class TraineeDetails extends React.Component {
                this.setState({ viewPauseMembershipModal: false, isPauseLoading: false })
             }
             else {
-               // alert(res.message)
+               alert(res.message)
                this.setState({ isPauseLoading: false })
             }
          })
          .catch((error) => {
-            // alert(error)
+            alert(error)
          })
    }
 
@@ -178,11 +180,11 @@ class TraineeDetails extends React.Component {
                this.fetchMemberList()
             }
             else {
-               // alert(res.message)
+               alert(res.message)
             }
          })
          .catch((error) => {
-            // alert(error)
+            alert(error)
          })
    }
 
